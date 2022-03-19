a4a4b85a23835876c3e5e7aea585106e06e0ead3
Mic <miscelo@tutanota.com>
2022-03-19 14:40:27 +0100

    userName.py created to start Project.

---
+++ userName.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+
+#Script to ask for username
+
+
+
+
+def getUsername():
+    user = input("Choose username please: ")
+    return user
+
+
+
+if __name__ == '__main__':
+    a = getUsername()
+    print(a)
